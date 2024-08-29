a2b441205db20bfee182f99716213a32f3b9bcda
Justin King <jlkingogie@knights.ucf.edu>
2024-08-29 16:26:05 -0400

    Initial commit - Basic C# Console Project

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      string wordle = "lover";
+      string guess;
+      bool solved = false;
+      // Playing the game loop
+      while (!solved)
+      {
+      // Getting valid input loop
+      do
+      {
+        // Prompts user to enter their 5 character guess
+        Console.WriteLine("Enter your 5 character guess:");
+        // Takes user input as guess, if null then fills in aaaaa
+        guess = Console.ReadLine() ?? "aaaaa";
+        guess = guess.Trim().ToLower();
+        if (guess.Length != 5)
+        {
+          Console.WriteLine("Invalid input. Please enter exactly 5 characters.");
+
+        }
+      } while (guess.Length != 5);
+      // Sets up answer stringbuilder
+      StringBuilder answer = new StringBuilder();
+
+
+    // Loops through the guess, matching with the correct wordle
+    for (int i = 0; i < wordle.Length; i++)
+    {
+      // if correct letter, put the correct letter
+      if (guess[i] == wordle[i])
+      {
+        answer.Append(wordle[i]);
+      }
+      // If incorrect letter, put a underscore
+      else{
+        answer.Append('_');
+      }
+    }
+    solved = wordle.Equals(answer.ToString());
+      // Displays their guess
+      Console.WriteLine("Your attempt is -> " + answer.ToString());
+    }
+    // Puzzle is solved!
+    Console.WriteLine("Great work!");
+
+    
+}
+}
